9fa77cd2f4b765050ad358f5bc8d31060b2bd441
JhonKill007 <jhond2069@gmail.com>
2022-07-06 23:15:15 -0400

    Followers Card

--- src/components/profileSide/ProfileSide.jsx
+++ src/components/profileSide/ProfileSide.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import FollowersCard from '../FollowersCard/FollowersCard';
 import LogoSearch from './LogoSearch/LogoSearch';
 
 const ProfileSide = () => {
   return (
     <div className='ProfileSide'>
       <LogoSearch/>
+      <FollowersCard/>
     </div>
   )
 }
