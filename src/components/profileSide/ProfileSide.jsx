cae93721e5503abf5f7800c785cb0a06a8efe307
David <Rafaeldavidrivas27@gmail.com>
2022-07-08 22:59:56 -0400

    PostShare errors solutionated

--- src/components/profileSide/ProfileSide.jsx
+++ src/components/profileSide/ProfileSide.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import ProfileCard from '../ProfileCard/ProfileCard'
 import FollowersCard from '../FollowersCard/FollowersCard';
 import LogoSearch from './LogoSearch/LogoSearch';
+import './ProfileSide.css'
 
 const ProfileSide = () => {
   return (
     <div className='ProfileSide'>
-      <LogoSearch/>
+      <LogoSearch/> 
+      <ProfileCard/>
       <FollowersCard/>
     </div>
   )
