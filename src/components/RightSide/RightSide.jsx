eb826a78a40cc12404355f2d7960da9aa1505dfb
Gian-P <pimentel.giancarlo1@gmail.com>
2022-07-11 18:31:02 -0400

    loaded the right side nav icons into the frontend

--- src/components/RightSide/RightSide.jsx
+++ src/components/RightSide/RightSide.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
-import './RightSide/'
+import React, { useState } from "react";
+import "./RightSide.css";
+import Home from "../../img/home.png";
+import Noti from "../../img/noti.png";
+import Comment from "../../img/comment.png";
+import { UilSetting } from "@iconscout/react-unicons";
+import TrendCard from "../TrendCard/TrendCard";
+/*
+import ShareModal from "../ShareModal/ShareModal";*/
 
-const ProfileSide = () => {
-}
+const RightSide = () => {
+  const [modalOpened, setModalOpened] = useState(false);
+  return (
+    <div className="RightSide">
+      <div className="navIcons">
+        <img src={Home} alt="" />
+        <UilSetting />
+        <img src={Noti} alt="" />
+        <img src={Comment} alt="" />
+      </div>
 
-export default ProfileSide
+      <TrendCard/>
+
+    </div>    
+  );
+};
+
+export default RightSide;
