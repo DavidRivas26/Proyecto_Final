9c4364be625511f04c65306b0836d1ea63cba751
jeremyarturo1 <79331844+jeremyarturo1@users.noreply.github.com>
2022-07-26 23:11:00 -0400

    Add files via upload
    Yo realize el Improving authentication page y el Setting up redux

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import store from "./store/ReduxStore"
 
 ReactDOM.render(
-  <App/>,
+  <provider store = {store}>
+  <App/>
+  </provider>,
   document.getElementById("root")
 );
-
-
